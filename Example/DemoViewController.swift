1ded5b5fefde7469b000d009f8c0e9dd6d2bc706
Ekhoo <me@lucas-ortis.com>
2015-12-18 07:32:55 +0100

    Add the initial project

---
+++ Example/DemoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DemoViewController.swift
+//  Translucid
+//
+//  Created by Lucas Ortis on 18/12/2015.
+//  Copyright © 2015 Ekhoo. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        
+        self.view.backgroundColor = UIColor.redColor()
+    }
+    
+}
