3838d09c9dbcf58f6cddfea36720fdf7c5490442
Lucas Ortis <lucas.ortis.external@watchever.com>
2016-08-10 10:18:24 +0200

    Enable shared framework

---
+++ TranslucidFramework/TranslucidFramework.h
@@ -0,0 +1,19 @@
+//
+//  TranslucidFramework.h
+//  TranslucidFramework
+//
+//  Created by Lucas Ortis on 10/08/2016.
+//  Copyright © 2016 Ekhoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TranslucidFramework.
+FOUNDATION_EXPORT double TranslucidFrameworkVersionNumber;
+
+//! Project version string for TranslucidFramework.
+FOUNDATION_EXPORT const unsigned char TranslucidFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranslucidFramework/PublicHeader.h>
+
+
